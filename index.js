00f5d2ec729f49e7990f9285d9d883784197dbb4
apple <davidhiggins1712@gmail.com>
2019-05-03 23:16:41 +0800

    added breadth first search method

--- index.js
+++ index.js
@@ -39,18 +39,72 @@ class SixDegreesOfFPL {
         const leagues1 = await res1.json()
         const leagues2 = await res2.json()
 
-        const league1 = await this.searchLeaguesForId(leagues1.leagues.classic, managerId2);
+        const league1 = await this.searchLeaguesForId(leagues1.leagues.classic, managerId2, leagues2.leagues.classic);
         if (league1) {
             console.log("found manager 1 in league: ", league1)
         } else {
-            console.log("not found")
+
         }
-        this.searchLeaguesForId(leagues2.leagues.classic, managerId1);
+        //this.searchLeaguesForId(leagues2.leagues.classic, managerId1);
     }
 
-    async searchLeaguesForId(leagues, id) {
-        if (leagues == null || leagues.length == 0) return false
+    async parseManagersLeagues(managerData) {
+        const data = await managerData.json();
+        const leagues = data.leagues.classic;
+        return leagues.filter(league => league.league_type !== "s");
+    }
 
+    /**
+     * breath-first search method to find id
+     */
+    async bfs(startingNodes, leaguesSecondManagerIsIn) {
+        let q = [];
+
+        for (let node of startingNodes) {
+            q.push([node, [node]])
+        }
+
+        let managerIdsVisited = new Set();
+        let leagueIdsVisited = new Set();
+        let secondManagerLeagues = new Set();
+        let degrees = 0
+
+        for (const league of leaguesSecondManagerIsIn) {
+            secondManagerLeagues.add(league.id);
+        }
+
+        while (q.length != 0) {
+            let [curr, path] = q.shift();
+            const fplManagerId = curr.entry;
+            if (!managerIdsVisited.has(fplManagerId)) {
+                managerIdsVisited.add(fplManagerId)
+
+                const managerData = await this.getManagersLeagues(fplManagerId);
+                const managerLeagues = await this.parseManagersLeagues(managerData);
+
+                for (const league of managerLeagues) {
+                    if (!leagueIdsVisited.has(league.id)) {
+                        leagueIdsVisited.add(league.id)
+
+                        if (secondManagerLeagues.has(league.id)) {
+                            //found path to other managers id
+                            console.log("found", path);
+                            return true;
+                        }
+                        const leagueMembers = await this.getLeagueMembers(league.id);
+                        for (const member of leagueMembers) {
+                            let newPath = [...path, member]
+                            q.push([member, newPath])
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    async searchLeaguesForId(leagues, id, leaguesSecondManagerIsIn) {
+        if (leagues == null || leagues.length == 0) return false
+        let possibleNodes = []
         for (const league of leagues) {
             // if they have league in common that is not a global league("s" is symbol for global)
             // global would be too easy to find as it has overall leaderboard
@@ -58,9 +112,14 @@ class SixDegreesOfFPL {
                 let members = await this.getLeagueMembers(league.id);
                 if (this.isPlayerMemberOfLeague(members, id)) {
                     return league;
+                } else {
+                    possibleNodes = [...possibleNodes, ...members]
                 }
             }
         }
+
+        //id wasnt found in any of the other managers leagues so perform bfs
+        await this.bfs(possibleNodes, leaguesSecondManagerIsIn);
         return null;
     }
 
